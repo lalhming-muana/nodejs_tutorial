c818670a0d3f0a5eecba0a7f3c45aca9c9854276
lhmuana <edd_vai@yahoo.co.in>
2022-12-10 19:20:11 +0530

    first commit for nodejs tutorial

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 10
+
+if(amount<10){
+    console.log('Small number');
+}
+else{
+    console.log('Large number');
+}
+
+console.log('Hey, this is my first app');
---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+GLOBALS  there are no windows only variables !!!!
+
+__dirname  - path to current directory
+
+__filename - filename
+
+require    - function to use modules(CommonJS)
+
+module     - info about current modules
+
+process    - info about env where the process is executed
+
+*/
+console.log(__dirname)
