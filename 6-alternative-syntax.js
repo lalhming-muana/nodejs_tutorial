d0df0e6e4f1a774a7e64005e14429e75fb9c25ab
lhmuana <edd_vai@yahoo.co.in>
2022-12-11 07:53:31 +0530

    tutorial is updated

---
+++ 6-alternative-syntax.js
@@ -0,0 +1,10 @@
+// This is another way of exporting something. It follows
+// a different syntax
+
+module.exports.items = ['item1','item2']
+
+const person ={
+    name: 'Bob'
+}
+
+module.exports.singlePerson = person
